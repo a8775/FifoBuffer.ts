e6ccb3a29214b1a696b15d3a916559a220409552
unknown <a8775@users.noreply.github.com>
2016-03-12 22:24:10 +0100

    tests and comments

--- FifoBuffer.ts
+++ FifoBuffer.ts
@@ -1,3 +1,24 @@
+
+/**
+ * FifoBuffer exception type
+ * 
+ * @export
+ * @class FifoBufferException
+ * @implements {Error}
+ */
+export class FifoBufferException implements Error {
+    public name: string = "FifoBufferException";
+    public message: string = "";
+
+    constructor(m: string) {
+        this.message = m;
+    }
+
+    public toString(): string {
+        return "{" + this.name + ": " + this.message + "}";
+    }
+}
+
 /**
  * Implementation of FIFO based on Uint8Array.
  * 
@@ -6,23 +27,23 @@
  */
 export class FifoBuffer {
     /**
-     * (description)
+     * Actual buffer to store data
      * 
      * @private
      * @type {Uint8Array}
      */
     private buffer: Uint8Array;
-    
+
     /**
-     * (description)
+     * Low index where data starts in buffer
      * 
      * @private
      * @type {number}
      */
     private bufferLowIndex: number;
-    
+
     /**
-     * (description)
+     * High index where data ends in buffer
      * 
      * @private
      * @type {number}
@@ -41,10 +62,21 @@ export class FifoBuffer {
     }
 
     /**
-     * (description)
+     * Check index n is in data range of FifoBuffer.
      * 
-     * @param {Uint8Array} data (description)
-     * @returns {void} (description)
+     * @param  {number} n
+     */
+    private assertIDX(n: number): void {
+        if (n < 0)
+            throw new FifoBufferException("Index out of range: index has to be greater than 0!");
+        if ((this.bufferLowIndex + n) > this.bufferHighIndex)
+            throw new FifoBufferException("Index out of range: index has to be lower than length!");
+    }
+
+    /**
+     * Push data to the end of FifoBuffer
+     * 
+     * @param {Uint8Array} data data to be pushed
      */
     public pushback(data: Uint8Array): void {
         if ((this.bufferHighIndex + data.byteLength) > this.buffer.byteLength) {
@@ -55,29 +87,32 @@ export class FifoBuffer {
             this.bufferHighIndex = this.bufferHighIndex - this.bufferLowIndex;
             this.bufferLowIndex = 0;
             this.pushback(data);
-            return;
         }
-
-        this.buffer.set(data, this.bufferHighIndex);
-        this.bufferHighIndex = this.bufferHighIndex + data.byteLength;
+        else {
+            this.buffer.set(data, this.bufferHighIndex);
+            this.bufferHighIndex = this.bufferHighIndex + data.byteLength;
+        }
     }
 
     /**
-     * (description)
+     * Pop from front of FifoBuffer
      * 
-     * @param {number} num (description)
-     * @returns {void} (description)
+     * @param {number} num number of bytes to pop from front of FifoBuffer
+     * @returns {Uint8Array} resulting buffer
      */
-    public popfront(num: number): void {
+    public popfront(num: number): Uint8Array {
+        this.assertIDX(num);
+        let ret = this.buffer.subarray(this.bufferLowIndex, this.bufferLowIndex + num);
         this.bufferLowIndex = this.bufferLowIndex + num;
         if (this.bufferLowIndex >= this.bufferHighIndex) {
             this.bufferHighIndex = 0;
             this.bufferLowIndex = 0;
-            return;
         }
 
         if (this.bufferLowIndex >= 1024)
             this.reduce();
+
+        return ret;
     }
 
     /**
@@ -95,21 +130,51 @@ export class FifoBuffer {
      * inner buffer and freeing unused space, if possible and greater than 
      * configured threshold. 
      * 
-     * @private
+     * 
+     * TODO: shoud reduse the size od FifoBuffer, need to add sepecial parameter 
+     * for threshold when freeing memory. 
      */
-    private reduce(): void {
+    public reduce(): void {
         this.buffer.copyWithin(0, this.bufferLowIndex, this.bufferHighIndex);
         this.bufferHighIndex = this.bufferHighIndex - this.bufferLowIndex;
         this.bufferLowIndex = 0;
     }
 
     /**
-     * Get FifoBuffer data
+     * Peek reange of data from FifoBuffer
      * 
-     * @returns {Uint8Array} data
+     * @param {number} start start position of peeked range of data
+     * @param {number} end end position od peeked range of data
+     * @returns {Uint8Array} resulting buffer
      */
-    public get(): Uint8Array {
-        return this.buffer.subarray(this.bufferLowIndex, this.bufferHighIndex);
+    public peek(start: number, end: number): Uint8Array;
+    /**
+     * Peek a byte from FifoBuffer 
+     * 
+     * @param {number} idx index position of requested  byte
+     * @returns {number} resulting data (byte)
+     */
+    public peek(idx: number): number;
+    /**
+     * Peek all data from FifoBuffer
+     * 
+     * @returns {Uint8Array} resulting buffer
+     */
+    public peek(): Uint8Array;
+    public peek(idx?: any, end?: any): number | Uint8Array {
+        if (idx === undefined)
+            return this.buffer.subarray(this.bufferLowIndex, this.bufferHighIndex);
+        if (typeof idx !== "number")
+            throw new FifoBufferException("Unsupportet type in method peek for idx!");
+
+        this.assertIDX(idx);
+        if (end === undefined)
+            return this.buffer[this.bufferLowIndex + idx];
+        if (typeof end !== "number")
+            throw new FifoBufferException("Unsupportet type in method peek for idx!");
+
+        this.assertIDX(end);
+        return this.buffer.subarray(this.bufferLowIndex + idx, this.bufferLowIndex + end);
     }
 
     /**
--- test/FifoBuffer.test.ts
+++ test/FifoBuffer.test.ts
@@ -13,5 +13,141 @@ describe('FifoBuffer.ts tests...', function() {
             expect(b.length()).to.be.equal(0);
         });
     });
+
+    describe('Test size of single pushed data', function() {
+        let b: FifoBuffer = new FifoBuffer();
+        b.pushback( new Uint8Array(5));
+
+        it('length should be 5', function() {
+            expect(b.length()).to.be.equal(5);
+        });
+    });
+        
+    describe('Test size of two pushed data', function() {
+        let b: FifoBuffer = new FifoBuffer();
+        b.pushback( new Uint8Array(5));
+        b.pushback( new Uint8Array(4));
+        it('length should be 9', function() {
+            expect(b.length()).to.be.equal(9);
+        });
+    });
+    
+    describe('Test pushed data', function() {
+        let b: FifoBuffer = new FifoBuffer();
+        let l = 10000;
+        for(let i=0;i<l;i++){
+            let v = new Uint8Array(1);
+            v[0] = i%256;
+            b.pushback(v);
+        }
+        it('data should be equal idx%256', function() {
+            let a = b.peek();
+            for(let j=0;j<l;j++) {
+                expect(a[j]).to.be.equal(j%256);
+            }
+        });
+        it('length should be ' + l, function() {
+            expect(b.length()).to.be.equal(l);
+        });
+        it('b[0] should be 0', function() {
+            expect(b.peek()[0]).to.be.equal(0);
+        });
+        it('b[100] should be 100', function() {
+            expect(b.peek()[100]).to.be.equal(100);
+        });
+        it('b[256] should be 0', function() {
+            expect(b.peek()[256]).to.be.equal(0);
+        });
+    });
+
+    describe('Test size of push and pop data', function() {
+        let b: FifoBuffer = new FifoBuffer();
+        b.pushback( new Uint8Array(5));
+        b.pushback( new Uint8Array(4));
+        b.popfront(5);
+        it('length should be 4', function() {
+            expect(b.length()).to.be.equal(4);
+        });
+    });
+    
+    describe('Test size of push, pop data and reduce', function() {
+        let b: FifoBuffer = new FifoBuffer();
+        b.pushback( new Uint8Array(5));
+        b.pushback( new Uint8Array(4));
+        b.popfront(5);
+        b.reduce();
+        it('length should be 4', function() {
+            expect(b.length()).to.be.equal(4);
+        });
+    });
+    
+    describe('Test size of push and clear', function() {
+        let b: FifoBuffer = new FifoBuffer();
+        b.pushback( new Uint8Array(5));
+        b.clear();
+        it('length should be 0', function() {
+            expect(b.length()).to.be.equal(0);
+        });
+    });
+    
+    describe('Test peek(idx) data', function() {
+        let b: FifoBuffer = new FifoBuffer();
+        let l = 1000;
+        for(let i=0;i<l;i++){
+            let v = new Uint8Array(1);
+            v[0] = i%256;
+            b.pushback(v);
+        }
+        it('length should be ' + l, function() {
+            expect(b.length()).to.be.equal(l);
+        });
+        it('data should be equal idx%256', function() {
+            let a = b.peek();
+            for(let j=0;j<l;j++) {
+                expect(a[j]).to.be.equal(j%256);
+            }
+        });
+        it('b[0] should be 0', function() {
+            expect(b.peek(0)).to.be.equal(0);
+        });
+        it('b[100] should be 100', function() {
+            expect(b.peek(100)).to.be.equal(100);
+        });
+        it('b[256] should be 0', function() {
+            expect(b.peek(256)).to.be.equal(0);
+        });
+    });
+
+    describe('Test peek(idx,end) data', function() {
+        let b: FifoBuffer = new FifoBuffer();
+        let l = 1000;
+        for(let i=0;i<l;i++){
+            let v = new Uint8Array(1);
+            v[0] = i%256;
+            b.pushback(v);
+        }
+        
+        let b2 = b.peek(5,10);
+        it('length should be 5', function() {
+            expect(b2.byteLength).to.be.equal(5);
+        });
+        
+        it('b2[0] should be 5', function() {
+            expect(b2[0]).to.be.equal(5);
+        });
+        it('b2[1] should be 6', function() {
+            expect(b2[0]).to.be.equal(5);
+        });
+        it('b2[2] should be 7', function() {
+            expect(b2[0]).to.be.equal(5);
+        });
+        it('b2[3] should be 8', function() {
+            expect(b2[0]).to.be.equal(5);
+        });
+        it('b2[4] should be 9', function() {
+            expect(b2[0]).to.be.equal(5);
+        });
+    });
+
 });
 
